e462f57c6c255ed7d1752098cf3d22d701ea50de
CodeInChaos <CodeInChaos@nowhere>
2010-09-11 15:07:40 +0200

    Member From Lamda initial

---
+++ Chaos.Util/Reflection/MemberFromLamda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Linq.Expressions;
+
+namespace Chaos.Util.Reflection
+{
+	public static class MemberFromLamda
+	{
+		public static MemberInfo MemberInfo(Expression exp)
+		{
+			return null;
+		}
+
+		public static PropertyInfo PropertyInfo(Expression exp)
+		{
+			return (PropertyInfo)MemberInfo(exp);
+		}
+	}
+}
